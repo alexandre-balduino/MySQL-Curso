5a2249154847943444916b719059afd66f47a30f
* <alexandrebalduino17@gmail.com>
2026-02-16 06:26:28 -0300

    Resolvi o exercício 13

--- exercicio_13.sql
+++ exercicio_13.sql
@@ -4,3 +4,10 @@ quantas pessoas pesam mais de 100kg
 e que estão acima da média de altura 
 de todos os cadastrados
 
+SELECT altura, COUNT(*)
+FROM gafanhotos
+WHERE peso > 100
+GROUP BY altura 
+HAVING altura > ( 
+SELECT AVG(altura) FROM gafanhotos 
+);
